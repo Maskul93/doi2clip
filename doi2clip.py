5bf5ac657cf5dcf9d49311c12429fc3e1b6025c0
Guido Mascia <maskul93@gmail.com>
2026-02-03 19:15:59 -0500

    Update main routine

--- doi2clip.py
+++ doi2clip.py
@@ -3,7 +3,7 @@ import json
 import pyperclip
 import sys
 import io
-from termcolor import colored
+import json 
 
 # required for converting months from numbers into strings
 months = {1: 'jan', 2: 'feb', 3: 'mar', 4: 'apr', 5: 'may', 6: 'jun',
@@ -43,50 +43,75 @@ def arrangeAuthors(authors):
 
     return auth_list
 
+def extractInfo(metadata):
+    # First extract all common information
+    authors = arrangeAuthors(metadata['author'])                                # author names
+    pub_date = metadata['published-print']['date-parts'][0]
+    year = pub_date[0]                                                          # year of publication
+    uniqueID = metadata['author'][0]['family'] + str(year)                      # uniqueID is the surname of the first author followed by the publication year
+
+    if len(pub_date) > 1:
+        month = months[pub_date[1]]         # if present, month of publication
+    else:
+        month = ''
+
+    title = metadata['title']                                                   # main title
+    url = metadata['URL']                                                       # url to doi
+    doi_meta = metadata['DOI']                                                  # doi without url
+    
+    ## \* Take cases one-by-one
+    if metadata['type'] == 'journal-article':
+        doc_type = types[metadata['type']]
+        volume = metadata['volume']
+        issn = metadata['ISSN'][-1]
+        
+        journal = metadata['container-title']
+        publisher = metadata['publisher']
+        pages = metadata['page']
+
+        joined = "".join([f'@{doc_type}{{{uniqueID},\n', f'\t title = {{{title}}},\n', f'\t volume = {{{volume}}},\n', 
+            f'\t ISSN = {{{issn}}},\n', f'\t url = {{{url}}},\n', f'\t DOI = {{{doi_meta}}},\n', f'\t journal = {{{journal}}},\n', 
+            f'\t publisher = {{{publisher}}},\n', f'\t author = {{{authors}}},\n', 
+            f'\t year = {{{year}}},\n', f'\t month = {{{month}}},\n', f'\t pages = {{{pages}}}\n', '}'])
+
+    if metadata['type'] == 'proceedings-article':
+        doc_type = types[metadata['type']]
+        booktitle = metadata['event']
+        publisher = metadata['publisher']
+        pages = metadata['page']
+
+        joined = "".join([f'@{doc_type}{{{uniqueID},\n', f'\t title = {{{title}}},\n', f'\t url = {{{url}}},\n',
+                          f'\t DOI = {{{doi}}},\n', f'\t booktitle = {{{booktitle}}},\n', f'\t publisher = {{{publisher}}},\n',
+                          f'\t author = {{{authors}}},\n', f'\t year = {{{year}}},\n', f'\t month = {{{month}}},\n',
+                          f'\t pages = {{{pages}}},\n', '}'])
+
+    return joined
+
+
 doi = 'https://doi.org/10.1109/BSN63547.2024.10780647'
+doi = '10.1109/TNSRE.2025.3594540'
+#doi = '10.1109/MeMeA52024.2021.9478688'
 metadata = getMetadata(doi)
 
-print(metadata['title'])
-# doc_type = types[metadata['type']]
-# authors = arrangeAuthors(metadata['author'])
-# year = metadata['deposited']['date-parts'][0][0]
-# month = months[metadata['deposited']['date-parts'][0][1]]
-# # uniqueID is the surname of the first author followed by the publication year
-# uniqueID = metadata['author'][0]['family'] + str(year)
+with open('data.json', 'w') as f:
+    json.dump(metadata, f, indent=4)
 
-# title = metadata['title']
-# volume = metadata['volume']
-# issn = metadata['ISSN'][-1]
-# url = metadata['URL']
-# doi_meta = metadata['DOI']
-# journal = metadata['container-title']
-# publisher = metadata['publisher']
-# pages = metadata['page']
+info = extractInfo(metadata)
 
-# ## \* Initialize Buffer for copying to clipboard */ ##
-# original_stdout = sys.stdout
-# sys.stdout = io.StringIO()
-# ## \* Organize what will be printed */ ##
-# if doc_type == 'article':
-#   print(f'@{doc_type}{{{uniqueID},')
-#   print(f'\t title = {{{title}}},')
-#   print(f'\t volume = {{{volume}}},')
-#   print(f'\t ISSN = {{{issn}}},')
-#   print(f'\t url = {{{url}}},')
-#   print(f'\t DOI = {{{doi_meta}}},')
-#   print(f'\t journal = {{{journal}}},')
-#   print(f'\t publisher = {{{publisher}}},')
-#   print(f'\t author = {{{authors}}},')
-#   print(f'\t year = {{{year}}},')
-#   print(f'\t month = {{{month}}},')
-#   print(f'\t pages = {{{pages}}}')
-#   print('}')
+## \* Initialize Buffer for copying to clipboard */ ##
+original_stdout = sys.stdout
+sys.stdout = io.StringIO()
 
-# # ## \* Get the value from the buffer and reset stdout */ ##
-# # captured_output = sys.stdout.getvalue()
-# # sys.stdout = original_stdout
+## \* Print information */ ##
+print(info)
 
-# # ## \* Copy the captured output to the clipboard and print it also */ ##
-# # pyperclip.copy(captured_output)
-# # print(captured_output)
-# print(metadata['event'])
+## \* Get the value from the buffer and reset stdout */ ##
+captured_output = sys.stdout.getvalue()
+sys.stdout = original_stdout
+
+## \* Copy the captured output to the clipboard and print it also */ ##
+pyperclip.copy(captured_output)
+print(captured_output)
+
+# Debug lines
+#print(len(metadata['published-print']['date-parts'][0]), metadata['published-print']['date-parts'][0])
